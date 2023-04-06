4e07de963d266d8e3ff15f7bd5d3f3022cb912da
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.gitlab.com>
2023-04-06 10:25:48 +0300

    Fix .csproj files.

--- FilterByPredicate.Tests/ContainsDigitFilterTests.cs
+++ FilterByPredicate.Tests/ContainsDigitFilterTests.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using FilterByDigitPredicate;
 using NUnit.Framework;
 
-namespace FilterByPredicates.Tests
+namespace FilterByPredicate.Tests
 {
     [TestFixture]
     public class ContainsDigitFilterTests
