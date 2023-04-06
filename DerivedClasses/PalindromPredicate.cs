228a2a19b7542f2a9815add37b0d8b192eddf2fd
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.gitlab.com>
2023-04-06 14:38:13 +0300

    Add derived classes.

---
+++ DerivedClasses/PalindromPredicate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FilterByPredicate;
+
+namespace DerivedClasses
+{
+    /// <summary>
+    /// Represents an class filter of the array on a given digit.
+    /// </summary> 
+    public class PalindromicFilter : Filter
+    {
+        protected override bool IsMatch(int item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
